b65f0276d6dc356ad606775477538c2ebbec2e05
Andrea Funtò <dihedron.dev@gmail.com>
2022-11-24 12:09:25 +0100

    Remove unused transform code

--- satellite/utils.go
+++ satellite/utils.go
@@ -11,31 +11,6 @@ import (
 
 var ErrNotImplemented = errors.New("not implemented")
 
-// // FromStringerField picks a field (given its name) and then calls the type-specific
-// // String() method on the value.
-// func FromStringerField(field string) *transform.ColumnTransforms {
-// 	return &transform.ColumnTransforms{
-// 		Transforms: []*transform.TransformCall{
-// 			{
-// 				Transform: transform.FieldValueGo,
-// 				Param:     field,
-// 			},
-// 			{
-// 				Transform: AsString,
-// 				Param:     nil,
-// 			},
-// 		},
-// 	}
-// }
-
-// // AsString invokes the String() function on the hydrate item coming from transform data.
-// func AsString(ctx context.Context, d *transform.TransformData) (interface{}, error) {
-// 	if t, ok := d.Value.(fmt.Stringer); ok {
-// 		return t.String(), nil
-// 	}
-// 	return nil, fmt.Errorf("invalid data type: %T", d.Value)
-// }
-
 // setLogLevel changes the current HCLog level; this seems necessary as the
 // STEAMPIPE_LOG_LEVEL variable does not seem to be properly read by the plugins.
 func setLogLevel(ctx context.Context, d *plugin.QueryData) {
